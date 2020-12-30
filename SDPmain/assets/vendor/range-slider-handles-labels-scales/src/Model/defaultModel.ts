081c4029eff7ae88a16c664ece5d0aa500b5bdc2
empowering <youngk@u.sogang.ac.kr>
2020-12-30 21:19:15 +0900

    Apply .gitignore

--- SDPmain/assets/vendor/range-slider-handles-labels-scales/src/Model/defaultModel.ts
+++
@@ -1,8 +0,0 @@
-import { ModelState } from '../helpers/interfaces';
-
-export const defaultModel: ModelState = {
-  min: 10,
-  max: 50,
-  step: 2,
-  values: [20],
-};
--- SDPmain/assets/vendor/range-slider-handles-labels-scales/src/helpers/constants.ts
+++
@@ -1,8 +0,0 @@
-const constants = {
-  DIRECTION_VERTICAL: 'vertical',
-  DIRECTION_HORIZONTAL: 'horizontal',
-  TYPE_INTERVAL: 'interval',
-  TYPE_SINGLE: 'single',
-};
-
-export { constants };
