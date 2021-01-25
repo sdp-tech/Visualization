4f35614890be0b31a21551387fea55af93bd38e7
empowering <youngk@u.sogang.ac.kr>
2021-01-26 01:49:43 +0900

    Edit Popup style

--- SDPmain/assets/js/map.js
+++ SDPmain/assets/js/map.js
@@ -34,44 +34,27 @@ toggle_selectableOptgroup();
 // Get the modal
 var modal = document.getElementById("tutorial");
 
-// Get the button that opens the modal
-var btn = document.getElementById("tutorial_btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 currentIndex = 0;
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+function tutorial_on() {
   modal.style.display = "block";
   currentIndex = 0;
   showpage(currentIndex);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function popup_close(){
+    modal.style.display = "none";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = popup_close();
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    popup_close();
   }
-}
-
-function setCookie(name, value, expiredays) {
-    var date = new Date();
-    date.setDate(date.getDate() + expiredays);
-    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString();
-}
-
-function closePopup() {
-    if (document.getElementById("check").value) {
-        setCookie("popupYN", "N", 1);
-        self.close();
-    }
 }
 
 function tutorialpage(action) {
