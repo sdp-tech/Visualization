7330fa35a873ef0fe7f16b77afebaf70cb73e285
brock.with <brock.with@kakaoenterprise.com>
2021-01-29 17:20:02 +0900

    add index.js
    basic server setting

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.use('/assets', express.static('SDPmain/assets/'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res)=>res.render('index'))
+app.get('/inner-page', (req, res)=>res.render('inner-page'))
+
+
+app.listen(PORT, ()=>console.log(`listen on ${PORT}`))
