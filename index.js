e4f81b1469fa2a53b13738226756393d56c16dca
Leegilwon <kiwww125@u.sogang.ac.kr>
2021-03-27 16:57:55 +0900

    test for research

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('view engine', 'pug')
 app.get('/', (__, res)=>res.render('index'))
 app.get('/index', (__, res)=>res.render('index'))
 app.get('/inner-page', (__, res)=>res.render('inner-page'))
+app.get('/pinner-page', (__, res)=>res.render('pinner-page'))
 app.get('/m-inner-page', (__, res)=>res.render('m-inner-page'))
 app.get('/apis/update-data', (__, res) => {
   mongodb.getData().then(data=>{cached_json = data});
