17faf0bb98b3bf5492d66f08e5c5f2c5b3db8c11
suejjangxx <youngk@u.sogang.ac.kr>
2021-03-27 14:51:06 +0900

    retouch design

--- assets/js/map.js
+++ assets/js/map.js
@@ -159,10 +159,9 @@ function load_map(json, customOption) {
     // ***************************/
     // mymap = L.map('mapwrap', { zoomControl: false }).setView([35, 40], 2.5);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGluYTAwNyIsImEiOiJja2hmZDNvOTgwNnVrMnJsNG1sOWtzcXNoIn0.Do0MCp-8-o2cv5cl-A2sNQ',
-        {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            subdomains: 'abcd',
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
             tileSize: 512,
             noWrap: false,
             // zoom controller
