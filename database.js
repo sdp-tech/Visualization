329bad4e43bb00cb006cedbd9e3c1681b5537240
Leegilwon <kiwww125@u.sogang.ac.kr>
2021-05-03 14:41:47 +0900

    temp commit

--- database.js
+++ database.js
@@ -3,50 +3,47 @@ const assert = require('assert');
 const request = require('request')
 
 const url = 'mongodb://sdpygl:sdp_ygl@3.36.175.233:27017/admin';
-
 const dbName = 'visualization';
-let db, mapCollection;
+let db, wbCollection, mapCollection;
 
-MongoClient.connect(url, async function(err, client) {
+// http://api.worldbank.org/v2/country/all/?format=json&page=2
+const setIncomeGeograph = () => {
+  try {
+    wbCollection.find({}).forEach(async (element) => {
+      const query = {
+        "properties.country": element.name
+      }, newvalues = {
+        $set: {
+          "properties.income_group": element.incomeLevel.value,
+          "properties.geographical": element.region.value
+        }
+      };
+
+      await mapCollection.updateMany(query, newvalues, function (err, res) {
+        if (err) throw err;
+      });
+    });
+  } catch (e) {
+    print(e);
+  }
+}
+
+const getProjectData = async () => {
+  const prj_data = await mapCollection.find({}).toArray()
+  return prj_data
+}
+
+MongoClient.connect(url, async function (err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
- 
+
   db = client.db(dbName);
   mapCollection = db.collection('map')
   wbCollection = db.collection('wbcountry')
-  // Find some documents
-
-  getWBdb();
-  request.get('http://localhost:4000/apis/update-data')
+  
+  request.get("http://localhost:4000/apis/update-data")
 });
 
-// http://api.worldbank.org/v2/country/all/?format=json&page=2
-
-function getWBdb(){
-  try {
-    wbCollection.find({}).forEach(element => {
-      var query = {"properties.country":element.name};
-      var newvalues = { $set: {"properties.income_group": element.incomeLevel.value , "properties.geographical": element.region.value } };
-      // var newvalues = { $set: {"properties.countryID": element.id} };
-      mapCollection.updateMany(query, newvalues, function(err, res) {
-        if (err) throw err;
-        // console.log(element.name+ ' ' + res.result.nModified + " document(s) updated");
-      });
-    });
-    } catch(e) {
-      print(e);
-  }
-
-}
-
-const getData = () => {
-    return new Promise((resolve, __) => {
-      mapCollection.find({}).toArray(function(__, features){
-        return resolve(features);
-      })
-  });
-}
-
 module.exports = {
-  getData
+  getProjectData
 }
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const mongodb = require('./database')
 const express = require('express')
 const cors = require('cors')
+const middlewares = require('./middlewares')
 
 const app = express()
 const PORT = 4000
-var cached_json = ""
-var counter = function (req, res){
-  res.render('index', {
-    countryn : 94,
-    delayn : 351,
-    canceln : 50,
-    sector : 'Energy'
-    });
-}
+var projectInfos = Object()
 
 app.use(cors())
 app.use('/', express.static(__dirname+ '/node_modules/'));
@@ -20,17 +13,17 @@ app.use('/assets', express.static(__dirname+'/assets'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
 
-app.get('/', counter)
-app.get('/index', counter)
+app.get('/', middlewares.counter)
+app.get('/index', middlewares.counter)
 app.get('/inner-page', (__, res)=>res.render('inner-page'))
 app.get('/m-inner-page', (__, res)=>res.render('m-inner-page'))
 app.get('/terms', (__, res)=>res.render('terms'))
 
 app.get('/apis/update-data', (__, res) => {
-  mongodb.getData().then(data=>{cached_json = data});
+  mongodb.getProjectData().then(data=>{projectInfos = data});
   return res.redirect('/inner-page')
 })
 
-app.get('/apis/data', (__, res)=> res.json({"body":cached_json}))
+app.get('/apis/data', (__, res)=> res.json({"body":projectInfos}))
 
 app.listen(PORT, ()=>console.log(`listen on ${PORT}`))
