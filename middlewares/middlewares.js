d9fb49748868149ed547fdb495ad947213631b3e
Leegilwon <kiwww125@u.sogang.ac.kr>
2021-05-10 20:36:42 +0900

    remove comments

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -28,16 +28,6 @@ const counter = async (req, res, next) => {
     next()
 }
 
-
-// const counter = function (req, res) {
-//     res.render('index', {
-//         countryn: 94,
-//         delayn: 351,
-//         canceln: 50,
-//         sector: 'Energy'
-//     });
-// }
-
 module.exports = {
     counter
 }
