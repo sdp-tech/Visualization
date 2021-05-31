24e3ad70cdfb009e9ce6bfd3ec654cf23740f6a8
Leegilwon <kiwww125@u.sogang.ac.kr>
2021-06-01 01:10:15 +0900

    hotfix for syntax error

--- assets/js/map.js
+++ assets/js/map.js
@@ -228,7 +228,7 @@ function load_map(json, customOption) {
         legend = L.control({position: 'bottomright'});
         legend.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'info legend'),
-                grades = [0, 3, 6 ,8, 15, 18, 25],
+                grades = [0, 3, 6 ,8, 15, 18, 25]
         
             // loop through our density intervals and generate a label with a colored square for each interval
             for (var i = 0; i < grades.length; i++) {
